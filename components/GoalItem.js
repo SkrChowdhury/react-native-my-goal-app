a8978640fc48ba938633378868891d59a225dbae
Sakkar Chowdhury <sakkar.chowdhury@intellier.com>
2022-12-01 17:40:52 +0600

    add task modal added

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,13 +3,16 @@ import React from "react";
 
 function GoalItem(props) {
   return (
-    <Pressable onPress={props.onDeleteItem}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#210644" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+      >
         <Text style={styles.goalText}>
           {props.text}
         </Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
@@ -18,11 +21,11 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc"
   },
   goalText: {
-    color: "white"
+    color: "white",
+    padding: 8
   }
 });
