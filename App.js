d0de0d3c565b5dc1e968f39abdbe903d1833cdd4
Sakkar Chowdhury <sakkar.chowdhury@intellier.com>
2022-12-01 16:00:46 +0600

    state management done with useState hook

---
+++ App.js
@@ -0,0 +1,54 @@
+import { View, StyleSheet, FlatList } from "react-native";
+import React, { useState } from "react";
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
+
+const App = () => {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function addGoalHandler(enteredGoalText) {
+    setCourseGoals(currentCourseGoals => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, id: Math.random().toString() }
+    ]);
+  }
+
+  function deleteGoalHandler() {
+    console.log("Delete");
+  }
+  return (
+    <View style={styles.appContainer}>
+      <GoalInput onAddGoal={addGoalHandler} />
+
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={courseGoals}
+          renderItem={itemData => {
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16
+  },
+  goalsContainer: {
+    flex: 5
+  }
+});
